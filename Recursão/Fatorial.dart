e9ef63f2d11f789ef4e0ac577cc4f1741a97adc7
Samuel Ximenes <71338886+SamuelXimenes27@users.noreply.github.com>
2022-01-25 11:59:43 -0300

    fixed

--- Recursão/Fatorial.dart
+++ Recursão/Fatorial.dart
@@ -1,47 +1,17 @@
 import 'dart:io';
 
-void main() {
-
-  stdout.write("What's your name? ");
-  var name = stdin.readLineSync();
-
-  //clear
-  print("\x1B[2J\x1B[0;0H");
-  
-  print(salute(name));
-
+void main(){
+  stdout.write("Type your number: ");
+  int x = int.parse(stdin.readLineSync() ?? '');
+  print(fat(x));
 }
 
-void salute2(name){
-  print('How r u ' + name + '?');
-}
+fat(x){
+  if(x == 1){
+    return 1;
+  }
+  else{
+    return x * fat(x-1);
+  }
   
-void bye <String>(){
-  print('Ok, bye');
-}
-  
-salute(name){
-  print('Hey, ' + name + '!');
-
-  //sleep-clear
-  sleep(Duration(seconds:3));
-  print("\x1B[2J\x1B[0;0H");
-   
-  salute2(name);
-
-  //sleep-clear
-  sleep(Duration(seconds:3));
-  print("\x1B[2J\x1B[0;0H");
-    
-  print('(preparing to say bye...)');
-
-  //sleep-clear
-  sleep(Duration(seconds:3));
-  print("\x1B[2J\x1B[0;0H");
-    
-  bye();
-  
-  //sleep-clear
-  sleep(Duration(seconds:3));
-  print("\x1B[2J\x1B[0;0H");
 }
